d1a4ce3da7a36e401c01f6108e37214e49701d38
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-06-27 16:59:36 -0300

    rota de buscar cpf

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const customers = [];
  * id - uuid
  * statement  []
  */
+
 app.post("/account", (request, response) =>{
     const { cpf, name} = request.body;
 
@@ -34,5 +35,13 @@ app.post("/account", (request, response) =>{
     return response.status(201).send();
 
 });
+
+app.get("/statement/:cpf", (request, response) => {
+  const {cpf} = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+})
 
 app.listen(3333);
